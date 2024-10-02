54d073abd8f461261995f01f55a1324f0aabb900
Duy <buituannghia01101970@gmail.com>
2024-10-02 09:47:06 +0700

    Initialize project

---
+++ src/main/java/com/buihuuduy/book_rating/BookRatingApplication.java
@@ -0,0 +1,13 @@
+package com.buihuuduy.book_rating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookRatingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buihuuduy/book_rating/BookRatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buihuuduy.book_rating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRatingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
